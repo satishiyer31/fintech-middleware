09c0b2aface8421836360a78db8552a4390e596f
satishiyer31 <satishiyer31@gmail.com>
2022-06-08 14:00:07 -0600

    Added route for account info

---
+++ routes/accounts.js
@@ -0,0 +1,32 @@
+const accounts = require('express').Router()
+const {SPREADSHEET_ID} = process.env
+
+const {google} = require('googleapis')
+
+const auth = new google.auth.GoogleAuth({
+    keyFile: "google-credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+})
+
+const spreadsheetId = SPREADSHEET_ID
+
+accounts.get('/', async(req,res)=> {
+
+    const client = await auth.getClient();
+
+    //Instance of API
+    const googleSheets = google.sheets({version: "v4", auth: client });
+      
+    const getRows = await googleSheets.spreadsheets.values.get({
+      
+        auth,
+        spreadsheetId,
+        range: "Accounts",
+      });
+      console.log(getRows.data.values[1][1])
+      res.json(getRows.data.values);
+
+
+})
+
+module.exports = accounts
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 
 const finOffersRouter = require('./offers.js')
+const AccountRouter= require('./accounts.js')
 
 const app = express();
 
 app.use('/getOffers', finOffersRouter)
+app.use('/getAccounts',AccountRouter)
 
 module.exports = app;
