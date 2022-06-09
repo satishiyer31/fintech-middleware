5755b3862edec7075af1159a7b30c4c9aa581386
satishiyer31 <satishiyer31@gmail.com>
2022-06-09 09:49:00 -0600

    Endpoint with headers

---
+++ routes/accountsheader.js
@@ -0,0 +1,42 @@
+const accounts = require('express').Router()
+const {SPREADSHEET_ID} = process.env
+
+const {google} = require('googleapis')
+
+const auth = new google.auth.GoogleAuth({
+    keyFile: "google-credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+})
+
+const spreadsheetId = SPREADSHEET_ID
+
+accounts.get('/:email', async(req,res)=> {
+
+    const client = await auth.getClient();
+
+    //Instance of API
+    const googleSheets = google.sheets({version: "v4", auth: client });
+      
+    const getRows = await googleSheets.spreadsheets.values.get({
+      
+        auth,
+        spreadsheetId,
+        range: "Accounts",
+      });
+      const rows = getRows.data.values;
+      const filteredRows =[];
+      for (const row of rows) {
+        if (((row[0]) == req.params.email)|| ((row[0]) == 'Email')){
+            filteredRows.push(row)
+        }
+    }
+
+    res.json(filteredRows);
+
+    //   console.log(getRows.data.values[1][1])
+    //   res.json(getRows.data.values);
+
+
+})
+
+module.exports = accounts
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 
 const finOffersRouter = require('./offers.js')
 const AccountRouter= require('./accounts.js')
+const AccountRouterHeader = require('./accountsheader.js')
 
 const app = express();
 
 app.use('/getOffers', finOffersRouter)
 app.use('/getAccounts',AccountRouter)
+app.use('/getAccounts2',AccountRouterHeader)
 
 module.exports = app;
